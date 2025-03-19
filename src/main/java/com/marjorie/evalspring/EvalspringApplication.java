3150774548464a7ab9aa87a15dc5549a5e3f7aea
soler marjorie <soler.marjorie81@gmail.com>
2025-03-19 09:42:51 +0100

    Iitialisation project springboot - eval

---
+++ src/main/java/com/marjorie/evalspring/EvalspringApplication.java
@@ -0,0 +1,13 @@
+package com.marjorie.evalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marjorie/evalspring/EvalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marjorie.evalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
